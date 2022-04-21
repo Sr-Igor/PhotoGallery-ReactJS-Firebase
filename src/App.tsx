b37ef52a2ef2eade972911738915d245aa60f609
Igor De Sousa <sr.igor.dev@gmail.com>
2022-04-21 14:05:28 -0300

    Adiconado os arquivos bases em vite, arquivos de lib/firebase/styledComp/uuid, e formata de arquvios env

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as C from './App.styled'
+
+function App() {
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header></C.Header>
+
+        {/* {Updload Area} */}
+
+        {/* {Photos Area} */}
+
+      </C.Area>
+    </C.Container>
+  )
+}
+
+export default App
